13a9302ad0d00c90e1d7e0ac053e566705861ffc
hoanglong1011 <viethq@spaceaa.com>
2022-05-08 14:23:00 +0700

    Update app.js

--- app.js
+++ app.js
@@ -1,5 +1,5 @@
 const express = require('express')
 const app = express()
 
-app.get('/', (req, res) => res.send('Hello World!!!!'))
+app.get('/', (req, res) => res.send('Hi AWS!!!!'))
 app.listen(3000, () => console.log('Check log service!!!'))
